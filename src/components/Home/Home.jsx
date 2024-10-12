7c73d8f3bd3fc59d333466bdbe02543840103c7d
ranamiah221 <ranarasul21@gmail.com>
2024-10-12 11:13:09 +0600

    added spinner for loading waiting a data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Sidebar from "../Sidebar/Sidebar";
-
+import { Bars } from "react-loader-spinner";
 
 const Home = () => {
-    return (
-       <>
-        <div className="flex mb-5">
-            <div className="w-1/6">
-            <Sidebar></Sidebar>
-            </div>
-            <div className="w-5/6 mt-2 pl-2">
-                <Outlet></Outlet>
-            </div>
+  const navigation = useNavigation();
+  return (
+    <>
+      <div className="flex mb-5">
+        <div className="w-1/6">
+          <Sidebar></Sidebar>
         </div>
-            <Footer></Footer>
-       </>
-        
-    );
+        <div className="w-5/6 mt-2 pl-2">
+          {navigation.state === "loading" ? (
+            <Bars
+              height="80"
+              width="80"
+              color="#4fa94d"
+              ariaLabel="bars-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
+          ) : (
+            <Outlet></Outlet>
+          )}
+        </div>
+      </div>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default Home;
